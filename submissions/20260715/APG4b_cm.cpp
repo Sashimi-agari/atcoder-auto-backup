776c90ab80287fed73770d2ffe5778f177cf4605
Sashimi-agari <date.shokora@gmail.com>
2026-07-15 10:55:53 +0900

    Submission for APG4b_cm on 20260715

---
+++ submissions/20260715/APG4b_cm.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  
+  int i = 0;
+  
+  cout << "A:";
+  while(i < A){
+    cout << "]";
+    i++;
+  }
+  
+  cout << endl;
+  cout << "B:";
+  i = 0;
+  
+  while(i < B){
+    cout << "]";
