1944e02beb5758f8c42d31717f2c3aa59e789901
Sashimi-agari <date.shokora@gmail.com>
2026-07-15 10:55:44 +0900

    Submission for APG4b_cp on 20260715

---
+++ submissions/20260715/APG4b_cp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
+  bool a = true; // true または false
+  bool b = false; // true または false
+  bool c = true; // true または false
+
+  // ここから先は変更しないこと
+
+  if (a) {
+    cout << "At";
+  }
+  else {
+    cout << "Yo";
+  }
+
+  if (!a && b) {
+    cout << "Bo";
+  }
