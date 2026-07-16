2638610686d01eaeede33d91e8e9fa87a999cdc2
Sashimi-agari <date.shokora@gmail.com>
2026-07-16 15:47:58 +0900

    Debug

--- script/run.ts
+++ script/run.ts
@@ -93,8 +93,7 @@ const main = async () => {
   // 同じ問題への違う提出があった際に別々のファイルとして格納できるように提出数をもっておくcounter
   const counter: Record<string, number> = {};
   const browser = await chromium.launch({
-  headless: true,
-  args: ["--no-sandbox"],
+  headless: false,
   });
 
 const page = await browser.newPage();
